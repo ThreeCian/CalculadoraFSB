2764862ef877b4e946127912e117fde535cbc9e9
Catherine Estrada <catheestrada.r@gmail.com>
2020-08-26 20:49:09 -0500

    Calculadora 26 de agosto

---
+++ assets/script/script.js
@@ -0,0 +1,36 @@
+const number1 = document.getElementById('num1')
+
+const number2 = document.getElementById('num2')
+
+let resul = document.getElementById('resul')
+
+
+console.log(num1)
+
+function operador(ope) {
+
+    switch (ope) {
+        case "suma":
+            resul.textContent = number1.valueAsNumber + number2.valueAsNumber
+
+            break;
+
+        case "resta":
+            resul.textContent = number1.valueAsNumber - number2.valueAsNumber
+
+            break;
+
+        case "por":
+            resul.textContent = number1.valueAsNumber * number2.valueAsNumber
+
+            break;
+
+        default:
+            resul.textContent = number1.valueAsNumber / number2.valueAsNumber
+
+            break;
+
+
+
+    }
+}
